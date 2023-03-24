b053eb936d1963863e03e7b01085f7add37b4a23
unknown <emmanuelemafe@gmail.com>
2023-03-24 17:24:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>My Header Section</h1>
+      <p>this is the body</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For update and promo codes!</p>
+    </Modal>
+  </teleport>
+  <button @click.left="toggleModal">open modal</button>
+  <button @click.left="toggleModalTwo">open modal2</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data () {
+    return {
+      title: 'My First Vue App',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style>
+#app, .modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+button {
+  background: #bbb;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px;
+  font-size: 16px;
+  color: #333;
+}
+</style>
